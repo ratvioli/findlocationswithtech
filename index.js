2918bffa14568251245e5ef28c5bf506675bfc7f
Krishan T <krishan0818@gmail.com>
2021-07-30 20:17:06 -0400

    Started to implement OpenTripMap api

---
+++ index.js
@@ -0,0 +1,20 @@
+const apiKey = "";
+
+function apiGet(method, query) {
+  return new Promise(function(resolve, reject) {
+    var otmAPI =
+      "https://api.opentripmap.com/0.1/en/places/" +
+      method +
+      "?apikey=" +
+      apiKey;
+    if (query !== undefined) {
+      otmAPI += "&" + query;
+    }
+    fetch(otmAPI)
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(function(err) {
+        console.log("Fetch Error :-S", err);
+      });
+  });
+}

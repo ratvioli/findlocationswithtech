5b3d620bc3000f454f151caa0b2f481b8e1996f7
Krishan T <krishan0818@gmail.com>
2021-07-31 22:52:01 -0400

    In Progress of making the API show its info

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const matchList = document.getElementById ('list');
 
 const searchTown = searchText => {
     var zipCode = searchText
-        if(zipCode.length < 3)
+        // if zipCode.length between(x, 3, 5) {
+
+        // }
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + 'api' + 'country=US&codes=' + zipCode, 'true')
+    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + '704e3e80-f262-11eb-9b72-6d432c880f62' + '&country=US&codes=' + zipCode, 'true')
     xhr.send();
     xhr.onload = function()
     {
         var data = JSON.parse(xhr.responseText)
         var results = document.createElement("p")
-        results.innerHTML = data.results.[zipCode][0].city
+        var url = zipCode
+        var data = JSON.parse(xhr.responseText);
+        var result = data.results;
+        var location  = result[Object.keys(result)[0]]
+        results.innerHTML = location[0].city
         matchList.appendChild(results)
         console.log(data)
     } 

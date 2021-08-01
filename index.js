29832682430d496a43d3d8272ffc204367f19da3
Krishan T <krishan0818@gmail.com>
2021-08-01 00:18:02 -0400

    Finished most of the .js, need to make it so innerHTML clears itself; CSS Code Added

--- index.js
+++ index.js
@@ -7,19 +7,41 @@ const searchTown = searchText => {
 
         // }
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + '704e3e80-f262-11eb-9b72-6d432c880f62' + '&country=US&codes=' + zipCode, 'true')
+    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + '0236f9d0-f277-11eb-82a2-85426b662485' + '&country=US&codes=' + zipCode, 'true')
     xhr.send();
     xhr.onload = function()
     {
         var data = JSON.parse(xhr.responseText)
-        var results = document.createElement("p")
+        var results = document.createElement("h3")
         var url = zipCode
         var data = JSON.parse(xhr.responseText);
         var result = data.results;
         var location  = result[Object.keys(result)[0]]
-        results.innerHTML = location[0].city
+        results.innerHTML = 'Location Name: ' + location[0].city
         matchList.appendChild(results)
         console.log(data)
+
+        var province = document.createElement("h3")
+        province.innerHTML = 'County: ' + location[0].province
+        matchList.appendChild(province)
+
+        var state_en = document.createElement("h3")
+        state_en.innerHTML = 'State: ' + location[0].state_en
+        matchList.appendChild(state_en)
+
+        var latitude = document.createElement("h3")
+        latitude.innerHTML = 'Latitude: ' + location[0].latitude
+        matchList.appendChild(latitude)
+
+        var longitude = document.createElement("h3")
+        longitude.innerHTML = 'Longitude: ' + location[0].longitude
+        matchList.appendChild(longitude)
+
+
+
+
+
+
     } 
 
  };

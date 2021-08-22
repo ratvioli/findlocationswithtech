8bf85df6c8d2f8c7a76a78a76e7944864c838f98
Krishan T <krishan0818@gmail.com>
2021-08-22 06:34:36 -0400

    swtiched api key

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const matchList = document.getElementById ('list');
 const searchTown = searchText => {
     var zipCode = searchText
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + '0236f9d0-f277-11eb-82a2-85426b662485' + '&country=US&codes=' + zipCode, 'true')
+    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + 'e4fae600-0333-11ec-9150-0553f55ffe0f' + '&country=US&codes=' + zipCode, 'true')
     xhr.send();
     xhr.onload = function()
     {
@@ -17,6 +17,7 @@ const searchTown = searchText => {
         var location  = result[Object.keys(result)[0]]
         results.innerHTML = 'Location Name: ' + location[0].city
         matchList.appendChild(results)
+        console.log(data)
         
         var province = document.createElement("h3")
         province.innerHTML = 'County: ' + location[0].province
@@ -61,55 +62,6 @@ const searchTown = searchText => {
 
  
 // distance.html
-
-const location1 = document.getElementById('location1');
-const location2 = document.getElementById ('location2');
-
-const locationdiff = searchText2 => {
-    var location1 = searchText2
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/distance?apikey=0236f9d0-f277-11eb-82a2-85426b662485&code='+ location1 + '&compare=' + location2 + '&country=us')
-    xhr.send();
-    xhr.onload = function()
-    {
-        matchList.innerHTML =  "";
-        var data = JSON.parse(xhr.responseText)
-        var results = document.createElement("h3")
-        var url = location1
-        var data = JSON.parse(xhr.responseText);
-        var result = data.results;
-        var location  = result[Object.keys(result)[0]]
-        results.innerHTML = 'Location Name: ' + location[0].city
-        matchList.appendChild(results)
-        
-        var province = document.createElement("h3")
-        province.innerHTML = 'County: ' + location[0].province
-        matchList.appendChild(province)
-
-        var state_en = document.createElement("h3")
-        state_en.innerHTML = 'State: ' + location[0].state_en
-        matchList.appendChild(state_en)
-
-        var latitude = document.createElement("h3")
-        latitude.innerHTML = 'Latitude: ' + location[0].latitude
-        matchList.appendChild(latitude)
-
-        var longitude = document.createElement("h3")
-        longitude.innerHTML = 'Longitude: ' + location[0].longitude
-        matchList.appendChild(longitude)
-
-
-    } 
-
- };
-
-
-
-
-
-
-
-
 
 
 

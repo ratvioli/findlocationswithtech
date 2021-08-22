5cdffe48e099636a39307c0690eee7855e9e3465
Krishan T <krishan0818@gmail.com>
2021-08-22 06:25:07 -0400

    commit so I can push to vercel

--- postalinradius.js
+++ postalinradius.js
@@ -4,7 +4,7 @@ const matchList = document.getElementById ('list');
 const searchTown = searchText => {
     var zipCode = searchText
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/search?apikey=' + '0236f9d0-f277-11eb-82a2-85426b662485' + '&country=US&codes=' + zipCode, 'true')
+    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/radius?apikey=0236f9d0-f277-11eb-82a2-85426b662485&code='+ zipCode +'&radius=16.0934&country=us')
     xhr.send();
     xhr.onload = function()
     {
@@ -43,77 +43,6 @@ const searchTown = searchText => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
- 
-// distance.html
-
-const location1 = document.getElementById('location1');
-const location2 = document.getElementById ('location2');
-
-const locationdiff = searchText2 => {
-    var location1 = searchText2
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://app.zipcodebase.com/api/v1/distance?apikey=0236f9d0-f277-11eb-82a2-85426b662485&code='+ location1 + '&compare=' + location2 + '&country=us')
-    xhr.send();
-    xhr.onload = function()
-    {
-        matchList.innerHTML =  "";
-        var data = JSON.parse(xhr.responseText)
-        var results = document.createElement("h3")
-        var url = location1
-        var data = JSON.parse(xhr.responseText);
-        var result = data.results;
-        var location  = result[Object.keys(result)[0]]
-        results.innerHTML = 'Location Name: ' + location[0].city
-        matchList.appendChild(results)
-        
-        var province = document.createElement("h3")
-        province.innerHTML = 'County: ' + location[0].province
-        matchList.appendChild(province)
-
-        var state_en = document.createElement("h3")
-        state_en.innerHTML = 'State: ' + location[0].state_en
-        matchList.appendChild(state_en)
-
-        var latitude = document.createElement("h3")
-        latitude.innerHTML = 'Latitude: ' + location[0].latitude
-        matchList.appendChild(latitude)
-
-        var longitude = document.createElement("h3")
-        longitude.innerHTML = 'Longitude: ' + location[0].longitude
-        matchList.appendChild(longitude)
-
-
-    } 
-
- };
-
-
-
-
-
-
-
-
-
-
-
-// Dropdown menu
  function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -129,4 +58,4 @@ const locationdiff = searchText2 => {
         }
       }
     }
-  }
+  } 
